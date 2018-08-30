4ee2feec6ad0ceecc1b7d2cab6dfa295a470369f
Richard Feldman <richard.t.feldman@gmail.com>
2018-08-30 09:00:10 -0400

    Add some more tests.

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,4 +1,4 @@
-module Example exposing (..)
+module Example exposing (knownValues, reflexive)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, float, int, list, string)
@@ -22,6 +22,26 @@ knownValues =
             \_ ->
                 Iso8601.toTime "1970-01-01T00:00:00Z"
                     |> Expect.equal (Ok (Time.millisToPosix 0))
+        , test "toTime \"2012-04-01T00:00:00-05:00\" gives me 1333256400000" <|
+            \_ ->
+                Iso8601.toTime "2012-04-01T00:00:00-05:00"
+                    |> Expect.equal (Ok (Time.millisToPosix 1333256400000))
+        , test "toTime \"2012-11-12T00:00:00+01:00\" gives me 1352674800000" <|
+            \_ ->
+                Iso8601.toTime "2012-11-12T00:00:00+01:00"
+                    |> Expect.equal (Ok (Time.millisToPosix 1352674800000))
+        , test "-5:00 is an invalid UTC offset (should be -05:00)" <|
+            \_ ->
+                Iso8601.toTime "2012-04-01T00:00:00-5:00"
+                    |> Expect.err
+        , test "+1:00 is an invalid UTC offset (should be +01:00)" <|
+            \_ ->
+                Iso8601.toTime "2012-04-01T00:00:00+1:00"
+                    |> Expect.err
+        , test "Invalid timestamps don't parse" <|
+            \_ ->
+                Iso8601.toTime "2012-04-01T00:basketball"
+                    |> Expect.err
         , test "toTime \"1970-01-01\" gives me 0" <|
             \_ ->
                 Iso8601.toTime "1970-01-01"
